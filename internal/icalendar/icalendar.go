e68933af0d37259029c5328b769fd991dc8e3ed5
jus1d <artfa63@gmail.com>
2024-01-30 12:05:48 +0400

    chore: refactor

--- internal/icalendar/icalendar.go
+++ internal/icalendar/icalendar.go
@@ -2,13 +2,14 @@ package icalendar
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"qarwett/internal/schedule"
 	"time"
 )
 
-var PairPositionToMinutesFromDayStart = map[int]int{
+const CalendarsDir = ".calendars"
+
+var pairPositionToMinutesFromDayStart = map[int]int{
 	0: 480,
 	1: 585,
 	2: 690,
@@ -19,37 +20,47 @@ var PairPositionToMinutesFromDayStart = map[int]int{
 	7: 1225,
 }
 
-func ScheduleToFile(groupID int64, schedule schedule.WeekPairs) {
-	if _, err := os.Stat(".calendars"); os.IsNotExist(err) {
-		_ = os.Mkdir(".calendars", 0755)
+func WriteScheduleToFile(groupID int64, schedule schedule.WeekPairs) (string, error) {
+	if _, err := os.Stat(CalendarsDir); os.IsNotExist(err) {
+		_ = os.Mkdir(CalendarsDir, 0755)
 	}
-	filename := fmt.Sprintf(".calendars/%d.icalendar", groupID)
+	filename := fmt.Sprintf("%s/%d.ics", CalendarsDir, groupID)
 	file, err := os.Create(filename)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	defer file.Close()
 
-	writeSchedule(file, schedule)
+	err = writeSchedule(file, schedule)
+	if err != nil {
+		return "", err
+	}
+	return filename, nil
 }
 
-func writeSchedule(file *os.File, schedule schedule.WeekPairs) {
-	writeHeaderICS(file)
+func writeSchedule(file *os.File, schedule schedule.WeekPairs) error {
+	err := writeICalenderHeader(file)
+	if err != nil {
+		return err
+	}
 
 	for i := 0; i < len(schedule.Pairs); i++ {
 		day := schedule.Pairs[i]
 		date := schedule.StartDate.AddDate(0, 0, i)
 		for j := 0; j < len(day); j++ {
 			pair := day[j]
-			start := date.Add(time.Duration(PairPositionToMinutesFromDayStart[pair.Position]) * time.Minute)
-			writeEvent(file, pair, start)
+			start := date.Add(time.Duration(pairPositionToMinutesFromDayStart[pair.Position]) * time.Minute)
+			err = writeEvent(file, pair, start)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
-	writeFooterICS(file)
+	return writeICalenderFooter(file)
 }
 
-func writeEvent(file *os.File, pair schedule.Pair, startTime time.Time) {
+func writeEvent(file *os.File, pair schedule.Pair, startTime time.Time) error {
 	endTime := startTime.UTC().Add(95 * time.Minute)
 	content := fmt.Sprintf("BEGIN:VEVENT\n")
 	content += fmt.Sprintf("DTSTART:%s\n", startTime.UTC().Format("20060102T150405"))
@@ -59,7 +70,6 @@ func writeEvent(file *os.File, pair schedule.Pair, startTime time.Time) {
 		content += fmt.Sprintf(" Подгруппа: %d", pair.Subgroup)
 	}
 	content += "\n"
-	writeToFile(file, content)
 	content = fmt.Sprintf("LOCATION:%s в %s\n", schedule.FullPairTypes[pair.Type], pair.Place)
 	content += fmt.Sprintf("SUMMARY:%s", pair.Title)
 	if pair.Subgroup != 0 {
@@ -68,22 +78,20 @@ func writeEvent(file *os.File, pair schedule.Pair, startTime time.Time) {
 	content += "\n"
 	content += "END:VEVENT\n"
 
-	writeToFile(file, content)
+	return writeToFile(file, content)
 }
 
-func writeHeaderICS(file *os.File) {
+func writeICalenderHeader(file *os.File) error {
 	calendarName := "University Schedule"
 	content := fmt.Sprintf("BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN\nMETHOD:PUBLISH\nX-WR-CALNAME:%s\nX-WR-TIMEZONE:Europe/Samara\n", calendarName)
-	writeToFile(file, content)
+	return writeToFile(file, content)
 }
 
-func writeFooterICS(file *os.File) {
-	writeToFile(file, "END:VCALENDAR")
+func writeICalenderFooter(file *os.File) error {
+	return writeToFile(file, "END:VCALENDAR")
 }
 
-func writeToFile(file *os.File, content string) {
+func writeToFile(file *os.File, content string) error {
 	_, err := file.WriteString(content)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return err
 }
