6defab2dd256a5fcb99ad195cd78e0bdf297394b
jus1d <artfa63@gmail.com>
2024-02-06 18:53:56 +0400

    feat: add check existed calendars

--- internal/app/bot/handler/callback.go
+++ internal/app/bot/handler/callback.go
@@ -118,14 +118,24 @@ func (h *Handler) OnCallbackAddCalendar(u telegram.Update) {
 	groupID, _ := strconv.ParseInt(parts[1], 10, 64)
 	languageCode := parts[2]
 
-	calendarID, err := h.storage.CreateTrackedCalendar(groupID, languageCode)
+	calendar, err := h.storage.GetTrackedCalendar(groupID, languageCode)
 	if err != nil {
-		log.Error("Failed to create tracked calendar", sl.Err(err))
-		callback := telegram.NewCallback(u.CallbackQuery.ID, locale.PhraseError(locale.RU))
-		_, err = h.bot.Request(callback)
+		log.Error("Failed to get calendar", sl.Err(err))
+	}
+
+	var calendarID string
+	if calendar == nil {
+		calendarID, err = h.storage.CreateTrackedCalendar(groupID, languageCode)
 		if err != nil {
-			log.Error("Failed to send callback", sl.Err(err))
+			log.Error("Failed to create tracked calendar", sl.Err(err))
+			callback := telegram.NewCallback(u.CallbackQuery.ID, locale.PhraseError(locale.RU))
+			_, err = h.bot.Request(callback)
+			if err != nil {
+				log.Error("Failed to send callback", sl.Err(err))
+			}
 		}
+	} else {
+		calendarID = calendar.ID
 	}
 
 	log.Debug("Created tracked calendar", slog.String("id", calendarID))
