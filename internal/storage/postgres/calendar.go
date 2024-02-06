b0e0916ddd52383fbd9c751298e4c1f0c4eb2672
jus1d <artfa63@gmail.com>
2024-02-06 18:02:00 +0400

    chore: add getAllCalendars, Calendar structure

--- internal/storage/postgres/calendar.go
+++ internal/storage/postgres/calendar.go
@@ -14,3 +14,27 @@ func (s *Storage) CreateTrackedCalendar(groupID int64, languageCode string) (str
 
 	return id, err
 }
+
+func (s *Storage) GetAllTrackedCalendars() ([]Calendar, error) {
+	var calendars []Calendar
+	rows, err := s.db.Query("SELECT * FROM calendars")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var calendar Calendar
+		err = rows.Scan(&calendar.ID, &calendar.GroupID, &calendar.LanguageCode, &calendar.LanguageCode)
+		if err != nil {
+			return nil, err
+		}
+		calendars = append(calendars, calendar)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return calendars, nil
+}
