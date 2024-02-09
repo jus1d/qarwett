6864a5673979b20cb5ce1f57e06056b1f7373969
jus1d <artfa63@gmail.com>
2024-02-09 17:59:45 +0400

    test(#29): add other tests for user

--- internal/storage/postgres/user_test.go
+++ internal/storage/postgres/user_test.go
@@ -37,17 +37,33 @@ func TestStorage_User(t *testing.T) {
 		LanguageCode: "en",
 	}
 
+	exists := storage.IsUserExists(user.TelegramID)
+	if exists {
+		t.Errorf("user should not be exist")
+	}
+
 	got, err := storage.CreateUser(user.TelegramID, user.Username, user.FirstName, user.LastName, user.LanguageCode)
 	if err != nil {
 		t.Errorf("error creating user: %v", err)
 	}
-
 	if got.ID == "" {
 		t.Errorf("incorrect user's uuid: '%s'", got.ID)
 	}
-
 	if user.TelegramID != got.TelegramID || user.Username != got.Username || user.FirstName != got.FirstName || user.LastName != got.LastName || user.LanguageCode != got.LanguageCode {
 		t.Errorf("user saved incorrectly, some fields doesn't match")
+	}
+	if got.Stage != StageNone {
+		t.Errorf("user's stage is incorrect: expected: 0, got: %d", got.Stage)
+	}
+
+	exists = storage.IsUserExists(user.TelegramID)
+	if !exists {
+		t.Errorf("user should exist")
+	}
+
+	err = storage.UpdateUserLinkedGroup(user.TelegramID, 779900000, "6101-010101D")
+	if err != nil {
+		t.Errorf("error updating user's linked group: %v", err)
 	}
 
 	users, err = storage.GetAllUsers()
@@ -58,13 +74,23 @@ func TestStorage_User(t *testing.T) {
 		t.Errorf("expected len(users): 1, got: %d", len(users))
 	}
 
+	err = storage.UpdateUserStage(user.TelegramID, StageWaitingAnnouncementMessage)
+	if err != nil {
+		t.Errorf("error updating user's stage: %v", err)
+	}
+
 	got, err = storage.GetUserByTelegramID(user.TelegramID)
 	if err != nil {
 		t.Errorf("error getting user: %v", err)
 	}
-
 	if user.TelegramID != got.TelegramID || user.Username != got.Username || user.FirstName != got.FirstName || user.LastName != got.LastName || user.LanguageCode != got.LanguageCode {
 		t.Errorf("got user is incorrect, some fields doesn't match")
+	}
+	if got.Stage != StageWaitingAnnouncementMessage {
+		t.Errorf("user's stage is incorrect: expected: 1, got: %d", user.Stage)
+	}
+	if got.LinkedGroupID != 779900000 || got.LinkedGroupTitle != "6101-010101D" {
+		t.Errorf("incorrect linked group: expected ID: %d, got: %d\nexpected title: %s, git: %s", 779900000, got.LinkedGroupID, "6101-010101D", got.LinkedGroupTitle)
 	}
 
 	err = storage.DeleteUser(user.TelegramID)
@@ -83,5 +109,12 @@ func TestStorage_User(t *testing.T) {
 	got, err = storage.GetUserByTelegramID(user.TelegramID)
 	if err == nil {
 		t.Errorf("error should be. user was not in db")
+	}
+
+	content := "announcement message content"
+	storage.SetAnnouncementMessage(77991100, content)
+	gotContent, exists := storage.GetAnnouncementMessage(77991100)
+	if gotContent != content || !exists {
+		t.Errorf("error with saving announcement message")
 	}
 }
