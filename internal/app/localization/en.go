a5631307f17179ba3c2c6399d8522dbf30486a96
jus1d <artfa63@gmail.com>
2024-02-09 19:40:41 +0400

    chore: add messages to localization

--- internal/app/localization/en.go
+++ internal/app/localization/en.go
@@ -1,7 +1,33 @@
 package localization
 
+import (
+	"fmt"
+	"qarwett/pkg/random"
+)
+
 var EnglishLocalization = Locale{
-	Messages: Messages{},
+	Messages: Messages{
+		Greeting: "<b>Hello, here you can take a quick look at your schedule <s>and go get some sleep</s></b>\n\n" +
+			"👇Just type your group",
+		AnnouncementRequest:      "Send me an announcement message",
+		CantStartAnnouncement:    "An error occurred while creating an announcement!",
+		AnnouncementCompleted:    "<b>Announcement completely sent to all users</b>",
+		CantFoundYourGroup:       "☹️Can't found your group\n\nAdd your group <b>to favourites</b>, to use this command",
+		NoGroupFound:             "☹️There are no groups at your request",
+		ChooseGroup:              "🤔<b>Choose a group</b>",
+		NoScheduleFound:          "🚨<b>Can't found schedule!</b>",
+		NoChanges:                "No changes",
+		Error:                    "Error",
+		Success:                  "Success",
+		Cancelled:                "Cancelled",
+		FailedToCancel:           "Failed to cancel",
+		EmptyAnnouncementMessage: "Announcement message not found. Try again",
+		UseRestart:               "Restart needed -> <b>/start</b>",
+		AdminCommands: "<b>List of admin commands:</b>\n\n" +
+			"<b>/announce</b> - Send an announcement message to all users\n" +
+			"<b>/users</b> - View users counter",
+		languageCode: English,
+	},
 	Buttons: Buttons{
 		Today:       "Today",
 		Favourite:   "To Favourites",
@@ -12,18 +38,37 @@ var EnglishLocalization = Locale{
 	languageCode: English,
 }
 
+var phrasesForFreeDayEN = []string{
+	"<b>%d %s</b> - a day of idleness",
+	"<b>%d %s</b> even the alarm clock will be sleeping",
+	"Plan for <b>%d %s</b> - just have no plans",
+	"<b>%d %s</b> is a perfect day to head to Sochi",
+	"<b>%d %s</b> - I allow you to be lazy",
+	"<b>%d %s</b> - a day of exploration, investigate where the second socks constantly disappear",
+	"Although it's <b>%d %s</b> off, stop lying on the couch",
+}
+
 func aboutEN(commit string) string {
-	return ""
+	if commit == "" {
+		commit = "xx"
+	}
+
+	return "<b>qarweTT</b> - take a quick look at schedule, and go back to sleep\n\n" +
+		"With any questions, you can write me <b>@jus1d</b>\n\n" +
+		"Current build version: <b>" + commit + "</b>"
 }
 
 func announcementCheckEN(content string) string {
-	return ""
+	return "<b>Your announcement message:</b>\n\n" + content + "\n\n" +
+		"Is everything correct?"
 }
 
 func usersAmountEN(amount int) string {
-	return ""
+	return fmt.Sprintf("<b>Total users:</b> %d", amount)
 }
 
 func freeDayEN(day int, month int) string {
-	return ""
+	monthsEN := []string{"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+
+	return fmt.Sprintf(random.Choice(phrasesForFreeDayEN), day, monthsEN[month])
 }
